feaaf59190739dab0093671d7d8c683f8f9c1848
unknown <purweaj766@gmail.com>
2022-01-06 20:02:18 +0545

    first day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Mid from './components/Mid';
 
 function App() {
+  const username = "sita";
+  const age = 24324;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Mid></Mid>
+      <footer>jfl fldj fljl fla</footer>
     </div>
-  );
+  )
 }
 
 export default App;
+
---
+++ src/components/Mid.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Mid extends Component {
+    render() {
+        return (
+            <div>this is mid section</div>
+        )
+    }
+}
+export default Mid;
