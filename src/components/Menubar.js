d53e2c07909aa72ba5ced50fdf084382217c1ed1
unknown <purweaj766@gmail.com>
2022-01-09 16:23:24 +0545

    working on state to direct display data

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 
 export default class Menubar extends Component {
     render() {
+        this.state = {
+            "quantity": 0,
+            "price" : "30 rs"
+        }
         return (
             <div>
-              this is Menubar  
+              <p>this is menu</p>  
+              <p>quantity : {this.state.quantity}</p> 
+              <p>Price : {this.state.price}</p> 
             </div>
         )
     }
