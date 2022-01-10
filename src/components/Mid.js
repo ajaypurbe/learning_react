a229ef731e745bdf0ece9e5c9ff34c4a43036ad3
unknown <purweaj766@gmail.com>
2022-01-10 11:02:07 +0545

    every thing imported with created class

---
+++ src/components/Mid.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Mid extends Component {
+    render() {
+        return (
+            <div>
+                <p>this is mmid</p>
+            </div>
+        )
+    }
+}
