675b8fb4a762dbfb303284795c34ff25c5e867d0
unknown <purweaj766@gmail.com>
2022-01-10 11:54:00 +0545

    working on route without refreshing the page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import {Route, Routes} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
         return (
             <div>
-                <a href='/about'>About</a>
+               <Link to="/">Home</Link>
+               <br></br>
+               <Link to="/about">About</Link>
+               <br></br>
+               <Link to ="/contact">Contact</Link>
             </div>
         )
     }
--- src/components/Mid.js
+++ src/components/Mid.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Home from './Home'
 import About from './About'
+import Contact from './Contact'
 import { Route, Routes } from 'react-router-dom'
 
 export default class Mid extends Component {
@@ -10,6 +11,7 @@ export default class Mid extends Component {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path= "/about" element={<About/>} />
+                    <Route path = "/contact" element={<Contact/>}/>
                 </Routes>
             </div>
         )
